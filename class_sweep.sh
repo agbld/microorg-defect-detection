54fb13422445d5ccd52973634f9a1dfa3432f450
agbld <agiblida@gmail.com>
2024-10-06 01:24:39 +0800

    create class_sweep.sh

---
+++ class_sweep.sh
@@ -0,0 +1,5 @@
+# Separately train models for each class: 
+for class in led particle flip Particle_Big marked tilt led_ng; do
+    python prepare_dataset.py --coco_json ./data/annotations/instance.json --images_dir ./data/images --yolo_dir ./yolo_dataset --train_ratio 0.8 --included_classes $class
+    python main.py --data_config ./yolo_dataset/data.yaml --model yolo11x --epochs 100 --batch_size 16 --run_name ${class}-yolo11x
+done
